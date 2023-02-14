5760014ef091ed949fd8cfba02c2b89e65089eea
ShoutingTable <Christopheremanuel97@gmail.com>
2023-02-14 01:23:47 -0600

    yes

--- split_map.py
+++ split_map.py
@@ -1,2 +1,20 @@
-A, B, C = map(int, input("Enter 3 numbers: ").split())
-print(A, B, C)
+# A, B, C = map(int, input("Enter 3 numbers: ").split())
+# print(A, B, C)
+
+
+
+# EXAMPLE WITH INTEGERS
+
+# A, B = map(int, input("Enter two numbers: ").split())
+# C, D, E = map(int, input("Enter three numbers: ").split())
+# F, G, H, I = map(int, input("Enter four numbers:").split())
+
+# print(A, B, C, D, E, F, G, H, I)
+
+
+# EAMPLE WITH STRINGS
+
+# A, B = input("Enter two words: ").split()
+# C, D, E = input("Enter three words: ").split()
+# F, G, H, I = input("Enter four words: ").split()
+# print(A, B, C, D, E, F, G, H, I)
